edfd07146db31f552e9ce2a01f726498980cb199
Gael Yasunari Ishimi <yasunarishimi@gmail.com>
2026-02-11 09:56:31 +0100

    feature(crustaceo-cascarudo/sprints#79): feature/chatbot

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,28 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from "./components/nav-bar/navbar";
 import { Footer } from "./components/footer/footer";
+import { CButton } from "./components/ui/c-button/c-button";
+import { AuthService } from './services/auth-service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar, Footer],
+  imports: [RouterOutlet, NavBar, Footer, CButton],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('front-user');
+
+  authService = inject(AuthService);
+  sanitizer = inject(DomSanitizer);
+
+  chatBotUrl: string = "https://landbot.online/v3/H-3316193-TRFFWKXKMYOIDATO/index.html" + `?isloggedin=${this.authService.isLoggedIn()}`;
+  showChatBot: boolean = false;
+
+
+  chatBotSanitizedUrl(){
+    return this.sanitizer.bypassSecurityTrustResourceUrl(this.chatBotUrl);
+  }
 }
