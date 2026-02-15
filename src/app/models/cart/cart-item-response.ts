ec4f0ff32c1ff3f1a2bf03057c5a47660166e686
Hugo <hugome2307@gmail.com>
2026-02-15 01:43:46 +0100

    wip

---
+++ src/app/models/cart/cart-item-response.ts
@@ -0,0 +1,9 @@
+export interface CartItemResponse {
+    id: number,
+    productId: number,
+    productName: string,
+    productImage: string,
+    quantity: number,
+    unitPrice: number,
+    subtotal: number
+}
