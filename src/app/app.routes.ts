76c8abf78943a1aa5b4a129dcd1bb5565f5ba106
HugoMengualAlcGVA <hugmenalc@alu.edu.gva>
2026-02-16 09:58:01 +0100

    wip, naming change and added to navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { PMenu } from './components/pages/p-menu/p-menu';
 import { PDeliveryAddress } from './components/pages/p-delivery-adress/p-delivery-address';
 import { PPayment } from './components/pages/p-payment/p-payment';
 import { paymentGuard } from './core/guards/payment-guard';
+import { POrders } from './components/pages/p-orders/p-orders';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -14,5 +15,6 @@ export const routes: Routes = [
   // {path: 'deals', component: },
   // {path: 'reservation', component: },
   {path: 'delivery', component: PDeliveryAddress},
-  {path: 'payment', component: PPayment, canActivate: [paymentGuard]}
+  {path: 'payment', component: PPayment, canActivate: [paymentGuard]},
+  {path: 'orders', component: POrders}
 ];
---
+++ src/app/components/pages/p-orders/p-orders.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'p-orders',
+  imports: [],
+  templateUrl: './p-orders.html',
+  styleUrl: './p-orders.scss',
+})
+export class POrders {
+
+}
